038ff61fb9cfecc14f5107438a4677a4c356700b
Rahat Reza <rahatreza3199@gmail.com>
2023-08-10 23:30:55 +0530

    dom main file

---
+++ animation.js
@@ -0,0 +1,13 @@
+var a=0;
+var id = setInterval(Anim,500);
+
+function Anim(){
+    a = a+10;
+    if(a ==200){
+        clearInterval(id);
+    }else{
+        var target = document.getElementById("test");
+        target.style.width = a + 'px';
+
+    }
+}
---
+++ dom_main.js
@@ -0,0 +1,32 @@
+var element;
+// to get a element 
+// element = document.getElementById("box").getAttribute("onclick")
+//element = document.getElementById("box").attributes[2]
+
+// to set an element
+
+
+// document.getElementById("box").innerHTML="<h1>Wow </h1>"
+// element = document.getElementById("box").innerHTML
+
+// type 1
+// document.getElementById("box").onclick = abc;
+
+
+// type 2
+
+// document.getElementById("box").addEventListener("mouseenter",abc)
+
+// document.getElementById("box").addEventListener('click',xyz)
+
+// document.getElementById("box").addEventListener("click",function(){
+//     this.style.border = "10px solid red"
+// })
+// function abc(){
+//     document.getElementById("box").style.background = 'green'
+// }
+
+// function xyz(){
+//     document.getElementById("box").removeEventListener('mouseenter',abc)
+// }
+// console.log(element)
