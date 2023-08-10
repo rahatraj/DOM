b74f5d14cee5f42176682e61a61ee3bfab371512
Rahat Reza <rahatreza3199@gmail.com>
2023-08-10 23:37:03 +0530

    domtravel

---
+++ domtravel.js
@@ -0,0 +1,31 @@
+//  ParentElement
+// document.getElementById("inner").parentElement.style.background = "red"
+
+// var a = document.getElementById("inner").parentElement;
+// console.log(a)
+
+// Children node
+
+// var a = document.getElementById("outer").children;
+// document.getElementById("inner").children[1].style.background = "red";
+// // var a = document.getElementById("inner").children[1];
+// var a = document.getElementById("inner").childNodes;
+// console.log(a)
+
+//  First Element Child
+
+// document.getElementById("outer").firstElementChild.style.background = "red"
+// var a = document.getElementById("outer").firstElementChild;
+// console.log(a)
+
+// last element child
+
+// document.getElementById("inner").lastElementChild.style.background = "red"
+// var a = document.getElementById("inner").lastElementChild;
+// console.log(a)
+
+
+// first child
+// document.getElementById("outer").firstChild.style.background = "red"
+var a = document.getElementById("inner").firstChild;
+console.log(a)
